5dd092e690aba87dcb7dc99465f44456673d7904
DESKTOP-E9817SU\admin <apimpale06@gmail.com>
2023-04-21 07:02:29 +0530

    initial commit

--- Datainputandoutput/input_out.cpp
+++ Datainputandoutput/input_out.cpp
@@ -1,2 +1,19 @@
 #include <iostream>
+int main(){
+    std::cout<<"hello"<<std::endl;
+    int age{21};
+    std::cout<<"age"<<age<<std::endl;
 
+    std::cerr<<"error"<<std::endl;
+    //
+    int age1;
+    std ::string name;
+    std::cout<<"enter your name and age"<<std::endl;
+    std::cin>>name;
+    std::cin>>age1;
+    std::getline(std::cin,name);
+    //std::getline(std::cin,name);
+    std::cout<<"hello "<<name<<" your age is "<<age1<<std::endl;
+
+    return 0;
+}
